b51c445f350233e4ca6c30b07335ae2f5e50b51f
Aaron <slapula@users.noreply.github.com>
2017-02-02 14:09:26 -0600

    Create noSingleSpace.js

---
+++ noSingleSpace.js
@@ -0,0 +1,37 @@
+Function.prototype.method = function(name, func) {
+  this.prototype[name] = func;
+  return this;
+};
+
+String.method('doubleSpace', function() {
+  var text = this.replace(/\u00a0(?!$)/g, " "); // unicode for &nbsp not at end of line
+  return text.replace(/[.|?|!]["|'|\u2019|\u201d]*\s{1,}(?!$)/g, function(a) { // unicode for curly quotes
+    var lastIndex = a[1] === "  " ? 2 : 3;
+    return a.substring(0, lastIndex);
+  });
+});
+
+function fixAllText(node){
+  if (node) {
+    node = node.firstChild;
+    while (node) {
+      if (node.nodeType == 3) {
+        var el = node.parentElement;
+        if (el.parentElement.hasAttribute('contenteditable')) { return false; };
+        node.textContent = node.textContent.doubleSpace();
+      } else {
+          fixAllText(node);
+        };
+      node = node.nextSibling;
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  fixAllText(document.body);
+});
+
+// poll for text subsequently added to page after DOM load and fix it
+window.setInterval(function() {
+  fixAllText(document.body);
+}, 100);
